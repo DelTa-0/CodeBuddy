2456faec9a8cb5b625156a33f505963470fe0dde
DelTa-0 <aryalmadhave123@gmail.com>
2025-07-09 16:44:57 +0545

    using text editor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,38 @@
 
 
 import './App.css'
+import Editor from "react-simple-code-editor"
 import "prismjs/themes/prism-tomorrow.css";
 import prism from "prismjs"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   useEffect(()=>{
     prism.highlightAll()
   })
-
+  const [code,setcode]=useState()
   return (
     <>
       <main>
         <div className="left">
           <div className="code">
-            <pre>
-              <code className="language-javascript">
-                {`function sum(){
-                  return 1+1;
-                }`}</code>
-            </pre>
+            <Editor value={code}
+            onValueChange={code=>setcode(code)}
+            highlight={code=>prism.highlight(code,prism.languages.javascript,"javascript")}
+            padding={10}
+            style={
+              {
+                fontFamily:"monospace",
+                fontSize:12,
+                border:"1px solid #ddd",
+                borderRadius:"5px",
+                height:"100%",
+                width:"100%",
+
+              }
+            }/>
+
+            
           </div>
           <div className="review">review</div>
         </div>
